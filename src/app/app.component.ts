10f74c47432a11fab1e549d944e688d9383fcc7b
quangvinh_911 <quangvinh00328@gmail.com>
2023-10-01 15:18:42 +0700

    convert string to boolean +1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-16-test';
+
+  runImmediately = false;
+
+  ngOnInit(): void {
+    this.runImmediately =  /^\s*(true)\s*$/i.test('0');
+    console.log(this.runImmediately);
+  }
 }
